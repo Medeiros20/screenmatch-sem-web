21dd791638cb99b82d6da2e37fbd3e4998e87599
João Medeiros <joaomanoelmedeirosmatos@gmail.com>
2025-12-09 22:39:45 -0300

    Implementação com banco de dados e uso de uma API da OPENIA

--- screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,4 +1,128 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(updatable = true)
+    private String titulo;
+    private Integer totaltemporadas;
+    private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
+    private  Categoria genero;
+    private String atores;
+    private String poster;
+    private String sinopse;
+
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
+
+    public Serie(){}
+
+    public Serie(DadosSerie dadosSerie){
+        this.titulo = dadosSerie.titulo();
+        this.totaltemporadas = dadosSerie.totaltemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0.0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        //this.sinopse = dadosSerie.sinopse();
+        //LINHA ABAIXO SERA UTILIZADA QUANDO UTILIZAR UMA API DE IA PARA TRADUÇÃO DE TEXTO
+        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotaltemporadas() {
+        return totaltemporadas;
+    }
+
+    public void setTotaltemporadas(Integer totaltemporadas) {
+        this.totaltemporadas = totaltemporadas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    @Override
+    public String toString() {
+        return  "genero = " + genero +
+                ", titulo = '" + titulo + '\'' +
+                ", totaltemporadas = " + totaltemporadas +
+                ", avaliacao = " + avaliacao +
+                ", atores = '" + atores + '\'' +
+                ", poster = '" + poster + '\'' +
+                ", sinopse = '" + sinopse + '\'';
+    }
 }
